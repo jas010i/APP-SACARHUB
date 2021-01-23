b9fc9e8c167164b70bcc53d372873606a78817b0
jas010i <63612124+jas010i@users.noreply.github.com>
2021-01-23 01:20:24 -0600

    big update

--- JS/geolocation.js
+++ JS/geolocation.js
@@ -1,67 +1,19 @@
-var drawingManager = new google.maps.drawing.DrawingManager();
-drawingManager.setMap(map);
-
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
-    const drawingManager = new google.maps.drawing.DrawingManager({
-      drawingMode: google.maps.drawing.OverlayType.MARKER,
-      drawingControl: true,
-      drawingControlOptions: {
-        position: google.maps.ControlPosition.TOP_CENTER,
-        drawingModes: [
-          google.maps.drawing.OverlayType.MARKER,
-          google.maps.drawing.OverlayType.CIRCLE,
-          google.maps.drawing.OverlayType.POLYGON,
-          google.maps.drawing.OverlayType.POLYLINE,
-          google.maps.drawing.OverlayType.RECTANGLE,
-        ],
-      },
-      markerOptions: {
-        icon:
-          "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-      },
-      circleOptions: {
-        fillColor: "#ffff00",
-        fillOpacity: 1,
-        strokeWeight: 5,
-        clickable: false,
-        editable: true,
-        zIndex: 1,
-      },
-    });
-    drawingManager.setMap(map);
-  }
-
-/* Data points defined as an array of LatLng objects */
-var heatmapData = [
-    new google.maps.LatLng(37.782, -122.447),
-    new google.maps.LatLng(37.782, -122.445),
-    new google.maps.LatLng(37.782, -122.443),
-    new google.maps.LatLng(37.782, -122.441),
-    new google.maps.LatLng(37.782, -122.439),
-    new google.maps.LatLng(37.782, -122.437),
-    new google.maps.LatLng(37.782, -122.435),
-    new google.maps.LatLng(37.785, -122.447),
-    new google.maps.LatLng(37.785, -122.445),
-    new google.maps.LatLng(37.785, -122.443),
-    new google.maps.LatLng(37.785, -122.441),
-    new google.maps.LatLng(37.785, -122.439),
-    new google.maps.LatLng(37.785, -122.437),
-    new google.maps.LatLng(37.785, -122.435)
-  ];
-  
-  var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
-  
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: sanFrancisco,
-    zoom: 13,
-    mapTypeId: 'satellite'
-  });
-  
-  var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: heatmapData
-  });
-  heatmap.setMap(map);
+    
+    
+    
+    
+    
+    
+    
+    
+    // Initialize and add the map
+    function initMap() {
+      // San Antonio, TX
+      const sanAntonio = { lat: 29.4241, lng: -98.4936 };
+      // The map, centered at San Antonio
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        mapId: '6cbaf94f194ffc7a', 
+        center: sanAntonio,
+      });
+    }
