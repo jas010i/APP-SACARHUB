ef1eeac758455d47ba92280d7a2f4a6e1b358623
jas010i <63612124+jas010i@users.noreply.github.com>
2021-01-24 01:29:37 -0600

    4

--- JS/geolocation.js
+++ JS/geolocation.js
@@ -11,13 +11,20 @@
     
     
     // Initialize and add the map
+
     function initMap() {
+    
       // San Antonio, TX
+    
       const sanAntonio = { lat: 29.4241, lng: -98.4936 };
+    
       // The map, centered at San Antonio
+    
       const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: sanAntonio,
-        mapId:'6cbaf94f194ffc7a',
-      });
+          zoom: 10,
+          center: sanAntonio,
+          mapId:'6cbaf94f194ffc7a',
+        });
     }
+
+    
